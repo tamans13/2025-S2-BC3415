11909a371ccbfdaca31d8b1e1f53a0ef6f3929b4
Tamanna <158043765+tamans13@users.noreply.github.com>
2026-03-16 08:24:35 +0000

    chatbot changes

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request
 import joblib
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 model = joblib.load("DBS_SGD_model.pkl")
 
@@ -23,6 +29,22 @@ def dbsPrediction():
     r = model.predict([[q]])
     r = r[0][0]
     return(render_template('dbsPrediction.html', r = r))
+
+@app.route("/chatbot", methods=["GET", "POST"])
+def chatbot():
+    return(render_template('chatbot.html'))
+
+@app.route("/reply", methods=["GET", "POST"])
+def reply():
+    q = request.form.get("q")
+    r = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[{
+        "role": "user",
+        "content": q
+    }]
+    )
+    return(render_template('reply.html', r = r.choices[0].message.content))
 
 if __name__ == "__main__":
     app.run()
